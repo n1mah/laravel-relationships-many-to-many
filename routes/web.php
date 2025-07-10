82f556a7f0453bd24d569c058da4c2cba1ad4793
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-10 15:58:21 +0330

    Feat : Add Fake Route For Test Create Item In Model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts', function () {
+    return Post::all();
+});
+
+Route::get('/tags', function () {
+    return Tag::all();
+});
+
+
+Route::get('/post/create', function () {
+  Post::create(['title'=>'title1', 'body'=>'This is body 1']);
+  Post::create(['title'=>'title2', 'body'=>'This is body 2']);
+});
+
+Route::get('/tag/create', function () {
+  Tag::create(['title'=>'sport']);
+  Tag::create(['title'=>'art']);
+  Tag::create(['title'=>'web']);
 });
