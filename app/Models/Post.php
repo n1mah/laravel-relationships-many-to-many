c10fd0ebd6938f6ffd96b555332837c18bb57baa
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-10 15:54:15 +0330

    Feat : Add Relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     protected $fillable = ['id', 'title','body'];
 
+    public function tags() : BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     protected $fillable = ['id', 'title'];
+
+     public function posts() : BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
